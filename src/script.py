2f7c3ee8e59cf8197135f07a6428e565b4a0114c
Roman <romdrak@email.cz>
2021-09-25 01:28:48 +0200

    Wannabe fix for the issue with the discarded local values

--- src/script.py
+++ src/script.py
@@ -2,21 +2,26 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from asyncio import wait_for   
+from asyncio import TimeoutError
 
 if TYPE_CHECKING:
     from chrono_api import ChronoApi
 
 
 class ChronoScript:    
-    def __init__(self, chrono_api: ChronoApi):
+    def __init__(self, chrono_api: ChronoApi, _locals: dict):
+        self.locals = _locals
         self.api = chrono_api
 
-    async def run(self):
-        from asyncio import wait_for   
-        from asyncio import TimeoutError
-        """ For some reason the imports get discarded, 
-        that's why we have to import them right into the function """
+    def _init_locals(self):
+        lx = globals()
+        for k, v in self.locals.items():
+            lx[k] = v
 
+    async def run(self):
+        self._init_locals()
+         
         try:
             await wait_for(self.api.walk(5), 2.)
         except TimeoutError:
