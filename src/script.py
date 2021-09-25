dd616f227fcb0063f02f73791087c6a2c5600878
Roman <romdrak@email.cz>
2021-09-25 13:13:59 +0200

    Remove unused methods and calls

--- src/script.py
+++ src/script.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-import threading
 
 from asyncio import wait_for   
 from asyncio import TimeoutError
@@ -10,16 +9,8 @@ if TYPE_CHECKING:
     import chrono_api
 
 
-class ChronoScript:    
-    def __init__(self) -> None:
-        ...
-
-    def handle_map_event(self, event: str) -> None:
-        print(threading.get_ident())
-        print(event)
-
+class ChronoScript: 
     async def run(self) -> None:
-        print(threading.get_ident())
         try:
             await wait_for(chrono_api.walk(5), 2.)
         except TimeoutError:
