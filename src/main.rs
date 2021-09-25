68af3a3691cbff93eb437405315eec5ca3e5b12c
Roman <romdrak@email.cz>
2021-09-25 13:09:56 +0200

    Canceling a future doesn't work :(

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use pyo3::prelude::*;
-use pyo3::types::{PyDict, PyModule, PyType};
-use pyo3::py_run;
+use pyo3::types::PyModule;
 use pyo3::wrap_pyfunction;
 
 
@@ -18,31 +17,26 @@ fn walk(py: Python, duration: i32) -> PyResult<&PyAny> {
 #[pyo3_asyncio::tokio::main]
 async fn main() -> PyResult<()> {
 
-
     let fut = Python::with_gil(|py| {
         let chrono_api = PyModule::new(py, "chrono_api")?;
         chrono_api.add_function(wrap_pyfunction!(walk, chrono_api)?)?;
 
-        let locals = PyDict::new(py);
-        locals.set_item("chrono_api", chrono_api)?;
-        
         let globals = py.import("__main__")?.dict();
         globals.set_item("chrono_api", chrono_api)?;
 
         py.run(&String::from_utf8(include_bytes!("./script.py").to_vec())?, Some(globals), None)?;
         let script = py.eval("SCRIPT()", Some(globals), None)?;
 
-        locals.set_item("__script_instance", script)?;
+        globals.set_item("__script_instance", script)?;
 
-        PyResult::Ok(vec![
-            pyo3_asyncio::tokio::into_future(py.eval("__script_instance.run()", None, Some(locals))?)?,
-            pyo3_asyncio::tokio::into_future(py.eval("__script_instance.run()", None, Some(locals))?)?,
-            pyo3_asyncio::tokio::into_future(py.eval("__script_instance.run()", None, Some(locals))?)?,
-            pyo3_asyncio::tokio::into_future(py.eval("__script_instance.run()", None, Some(locals))?)?,
-        ])
+        pyo3_asyncio::tokio::into_future(py.eval("__script_instance.run()", Some(globals), None)?)
     })?;
 
-    futures::future::join_all(fut).await;
+    let handle = tokio::spawn(fut);
+
+    handle.await.unwrap()?;
+
+    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
 
     Ok(())
 }
--- src/script.py
+++ src/script.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+import threading
 
 from asyncio import wait_for   
 from asyncio import TimeoutError
@@ -13,10 +14,12 @@ class ChronoScript:
     def __init__(self) -> None:
         ...
 
-    async def handle_map_event(self, event: str) -> None:
+    def handle_map_event(self, event: str) -> None:
+        print(threading.get_ident())
         print(event)
 
     async def run(self) -> None:
+        print(threading.get_ident())
         try:
             await wait_for(chrono_api.walk(5), 2.)
         except TimeoutError:
